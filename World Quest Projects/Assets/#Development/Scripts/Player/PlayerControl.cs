8f273988c60aedddb36e5d232cdf0ebae051ef08
Adithia Tirta Zulfikar <adithiatirtaz@gmail.com>
2022-09-17 23:31:27 +0700

    Add Script Player

---
+++ World Quest Projects/Assets/#Development/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
